d371196b149cee5d57835bec2aa9f5397292ed86
Yu Zhang <yzhang0713@gmail.com>
2022-08-14 00:43:26 -0400

    Add design controller, design view

---
+++ src/main/java/tacos/Order.java
@@ -0,0 +1,18 @@
+package tacos;
+
+import lombok.Data;
+import org.springframework.context.annotation.Primary;
+
+@Data
+public class Order {
+
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+}
